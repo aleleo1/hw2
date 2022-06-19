9763ebe76a7b9c286f4b63e5729dd9c184bdd788
aleleo1 <100156426+aleleo1@users.noreply.github.com>
2022-06-19 11:35:58 +0200

    Add files via upload

---
+++ storage/framework/views/1cf6c9ecfe97869d7b48e139f37494388485ff7a.php
@@ -0,0 +1,74 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/header.css">
+
+    <meta name="generator" content="Hugo 0.87.0">
+
+    <title>Hw2</title>
+
+
+
+    <!-- Bootstrap core CSS -->
+    <style>
+        body {
+
+            text-align: center;
+            font-family: 'arial', sans-serif;
+            background-color: beige;
+         /*    width: 100vw;
+            height:100vh; */
+            scroll-behavior: smooth;
+            scrollbar-color: brown bisque;
+            margin: auto;
+            display: flex;
+            flex-direction: column;
+            overflow-y: scroll;
+            overflow-x: hidden;
+            
+
+        }
+
+        main{
+            background-color: burlywood;
+            height: 100%;
+        }
+    </style>
+
+    <!-- Custom styles for this template -->
+
+
+
+</head>
+
+<body>
+
+
+
+    <?php echo $__env->make('layouts.includes.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+
+    <main>
+
+        <?php echo $__env->yieldContent('content'); ?>
+
+    </main>
+
+
+
+
+
+
+
+</body>
+
+</html>
+<?php /**PATH /home/alexleon/Desktop/hw2/hw2/resources/views/layouts/app.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/cd9cd54a143313ad4f2f121a9d6017eb1f2e208c.php
@@ -0,0 +1,61 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+
+    <meta name="generator" content="Hugo 0.87.0">
+
+    <title>Laravel 9 Custom Login and Resgisteration</title>
+
+ 
+
+    <!-- Bootstrap core CSS -->
+
+
+
+
+
+ 
+
+
+ 
+
+    <!-- Custom styles for this template -->
+
+   
+
+</head>
+
+ 
+
+<body class="text-center">
+
+ 
+
+    <main class="form-signin">
+
+ 
+
+        <?php echo $__env->yieldContent('content'); ?>
+
+ 
+
+    </main>
+
+
+ 
+
+</body>
+
+ 
+
+</html><?php /**PATH /home/alexleon/Desktop/hw2/hw2/resources/views/layouts/auth.blade.php ENDPATH**/ ?>
